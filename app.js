154620d25063da574651c0acbbf817c8607d4377
rinalfauzi <rinalfauzi@gmail.com>
2020-10-18 12:49:35 +0700

    Set Middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 //express app
 const app = express()
@@ -8,6 +9,12 @@ app.set('view engine', 'ejs')
 
 //listen for requests
 app.listen(3000)
+
+//middleware & static files
+app.use(express.static('public'))
+
+//this is middleware
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     //res.send('<p>Hello</p>')
